f68539ecd65201c92d7879b02b60710dda39450d
Ahmet Hakan YILDIRIM <hakan-336-ahmet@outlook.com>
2024-12-15 13:43:40 +0300

    Added AppStrings struct for centralized string management

---
+++ TicTacToe/TicTacToe/Utils/AppStrings.swift
@@ -0,0 +1,16 @@
+//
+//  AppStrings.swift
+//  TicTacToe
+//
+//  Created by AHMET HAKAN YILDIRIM on 15.12.2024.
+//
+
+import Foundation
+
+struct AppStrings {
+    static let vsCpu = "VS CPU"
+    static let vsHuman = "VS Human"
+    static let online = "Online"
+    static let appName = "Tic Tac Love"
+    
+}
